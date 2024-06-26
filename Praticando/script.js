4bc32892105167fa4da64a44fccd177447478994
lanocod <lanocodigo@gmail.com>
2024-06-26 12:42:54 -0300

    Initial commit

---
+++ Praticando/script.js
@@ -0,0 +1,8 @@
+const whatsappLink = document.getElementById('whatsapp-link');
+
+whatsappLink.addEventListener('click', function() {
+  const phoneNumber = '+5551982699454'; /
+  const url = `https://api.whatsapp.com/send?phone=${  const phoneNumber = '+5551982699454'; /
+  }`;
+  window.open(url, '_blank');
+});
